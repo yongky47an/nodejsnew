9c130d090cf6e2c7ccd2cd3fe66e1b4776ec5042
yongkytpfx <yongky.adityo@tpfx.co.id>
2024-10-30 23:51:53 +0700

    fix : home screen

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const apiCall = () => {
+    axios.get('http://127.0.0.1:5000/get_users').then((data) => {
+      
+    })
+  }
+
+ // const [name, ] = useState("");
 
 function Home() {
-  return <div>Home Page</div>;
+    return (
+        
+
+        <div className='App-header'>
+         <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <h1>
+         
+        </h1>
+        <div className="card">
+         
+          <h2>Sign In</h2>
+          <header className="App-header">
+
+            <button onClick={apiCall}>Make API Call</button>
+
+            </header>
+
+            <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+        />
+      </label>
+    </form>
+        </div>
+      </div>
+
+      );
 }
 
 export default Home;
